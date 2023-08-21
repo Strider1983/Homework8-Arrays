53289391adc14bac7be3fa03056e91275c2986db
Strider <mezentsev.k@gmail.com>
2023-08-21 21:45:08 +0800

    Задача №1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Массивы. Урок 1");
         System.out.println("_______________");
         System.out.println("Задача №1");
+        System.out.println("Массив 1");
         int [] numbers = new int [3];
         numbers [0] = 1;
         numbers [1] = 2;
@@ -11,16 +12,19 @@ public class Main {
         System.out.println(numbers [1]);
         System.out.println(numbers [2]);
         System.out.println(" ");
+        System.out.println("Массив 2");
         double [] fractionals = {1.57, 7.654, 9.986};
         System.out.println(fractionals [0]);
         System.out.println(fractionals [1]);
         System.out.println(fractionals [2]);
         System.out.println(" ");
+        System.out.println("Массив 3");
         double [] mass = {23, 7, 64, 35.8, 76.2, 11, 3.67};
         for (int i = 0; i < mass.length; i++) {
             System.out.println(mass [i]);
         }
         System.out.println("Длина массива - " + mass.length + " элементов");
-
+        System.out.println("_______________");
+        System.out.println("Задача №2");
     }
     }
