4100d21345e73591834084ec5d17c4f9e5c8e97f
Strider <mezentsev.k@gmail.com>
2023-08-21 22:47:02 +0800

    Задача №3 выполнена

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,24 @@ public class Main {
         System.out.println("_______________");
         System.out.println("Задача №1");
         System.out.println("Массив 1");
-        int [] numbers = new int [3];
-        numbers [0] = 1;
-        numbers [1] = 2;
-        numbers [2] = 3;
-        System.out.println(numbers [0]);
-        System.out.println(numbers [1]);
-        System.out.println(numbers [2]);
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
         System.out.println(" ");
         System.out.println("Массив 2");
-        double [] fractionals = {1.57, 7.654, 9.986};
-        System.out.println(fractionals [0]);
-        System.out.println(fractionals [1]);
-        System.out.println(fractionals [2]);
+        double[] fractionals = {1.57, 7.654, 9.986};
+        System.out.println(fractionals[0]);
+        System.out.println(fractionals[1]);
+        System.out.println(fractionals[2]);
         System.out.println(" ");
         System.out.println("Массив 3");
-        double [] mass = {23, 7, 64, 35.8, 76.2, 11, 3.67};
+        double[] mass = {23, 7, 64, 35.8, 76.2, 11, 3.67};
         for (int i = 0; i < mass.length; i++) {
-            System.out.println(mass [i]);
+            System.out.println(mass[i]);
         }
         System.out.println("Длина массива - " + mass.length + " элементов");
         System.out.println("_______________");
@@ -31,5 +31,28 @@ public class Main {
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(fractionals));
         System.out.println(Arrays.toString(mass));
+        System.out.println("_______________");
+        System.out.println("Задача №3");
+        for (int i1 = numbers.length - 1; i1 >= 0; i1 = i1 - 1) {
+            if (i1 == 0) {
+                System.out.println(numbers[i1]);
+                break;
+            }
+            System.out.print(numbers[i1] + ", ");
+        }
+        for (int i2 = fractionals.length - 1; i2 >= 0; i2 = i2 - 1) {
+            if (i2 == 0) {
+                System.out.println(fractionals[i2]);
+                break;
+            }
+            System.out.print(fractionals[i2] + ", ");
+        }
+        for (int i3 = mass.length - 1; i3 >= 0; i3 = i3 - 1) {
+            if (i3 == 0) {
+                System.out.println(mass[i3]);
+                break;
+            }
+            System.out.print(mass[i3] + ", ");
         }
     }
+}
